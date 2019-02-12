f31a9fffb7e9d2aafefb6112e4122addd40671c9
bohdans <0000xffff@gmail.com>
2019-02-12 18:45:53 +0200

    Autolayout - initial commit

---
+++ autolayout/sources/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2019 vinso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
